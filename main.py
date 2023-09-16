5884da6e1694d3e8fc4c20a336d8a0a1c4703821
lionelhost <dplionnel@gmail.com>
2023-09-16 13:01:48 +0000

    create connexion to db, create collections and dsb, and insert one brand

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+import pymongo
+
+
+myClient = pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2")
+
+db = myClient["car_mongo_db"]
+brands =  db["brands"]
+models = db["models"]
+cars = db["cars"]
+drivers = db["drivers"]
+
+
+br = brands.insert_one({
+    "name": "WV",
+    "country": "germany",
+    "description": "lorem ipsum dolor et",
+    "location": "Berlin"
+})
+
+print(br.inserted_id)
+
+
+
+
+
+
+
+
 print("Welcome to cars mongo project")
-
-print("Je viens de modifier le fichier")
-
-def add(a,b):
-    print(a+b)
